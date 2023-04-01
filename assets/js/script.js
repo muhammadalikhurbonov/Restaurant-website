3a8e776326ec0bd455341a85b80c0a9f78f30051
muhammadalikhurbonov <muhammadaliqurbonov551@gmail.com>
2023-04-01 19:57:10 +0500

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const Box = document.querySelector(".box");
+const Box = document.querySelector(".animation_box");
 const Animation = document.querySelector(".animation");
 
 function clickFunction() {
