76164fd1677c0dad8e256144ccd5b557c4cc2a8f
muhammadalikhurbonov <muhammadaliqurbonov551@gmail.com>
2023-04-02 19:59:28 +0500

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
-const Box = document.querySelector(".animation_box");
-const Animation = document.querySelector(".animation");
+const Animation_Box = document.querySelector(".animation_box");
+const Animation_Time = document.querySelector(".animation_time");
 const Exit = document.querySelector(".exit_on");
+const Form = document.getElementsByClassName("form");
 
 function clickFunction() {
-  Box.classList.add("Animation");
-  Box.classList.add("Exit");
+  Form.classList.add("Animation_Time");
+  Form.classList.add("Exit");
 }
 function ExitFunction() {
-  Box.classList.remove("Animation");
+  Form.classList.remove("Animation_Time");
 }
