1badfec6d3303b7be7e32aa3489bfc9a82178295
muhammadalikhurbonov <muhammadaliqurbonov551@gmail.com>
2023-04-03 22:49:10 +0500

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,29 @@
-const Animation_Box = document.querySelector(".animation_box");
-const Animation_Time = document.querySelector(".animation_time");
-const Exit = document.querySelector(".exit_on");
-const Form = document.getElementsByClassName("form");
+const Form = document.querySelector(".form");
+const Exit = document.querySelector(".exit_form");
+const inp = document.getElementById("username");
+const Login = document.getElementById("login");
 
 function clickFunction() {
-  Form.classList.add("Animation_Time");
-  Form.classList.add("Exit");
+  Form.classList.add("animation_time");
+  Exit.classList.add("exit_on");
+  document.querySelector(".body").style.background = "black";
+  document.getElementById("button").addEventListener("click", function (e) {
+    e.preventDefault();
+    document.getElementById("login").innerHTML = inp.value;
+    Form.classList.add("animation_time_off");
+    document.querySelector(".dropdown-content").style.display = "block";
+    document.cookie = inp.value;
+    window.localStorage.setItem("Name", inp.value);
+  });
+}
+// Login.innerHTML = window.localStorage.getItem("Name");
+function logOut() {
+  Login.innerHTML = "Sign In";
+  window.localStorage.clear();
+  document.querySelector(".dropdown-content").style.display = "none";
+  document.querySelector(".body").style.background = "white";
 }
 function ExitFunction() {
-  Form.classList.remove("Animation_Time");
+  Form.classList.remove("animation_time");
+  Exit.classList.add("exit_on");
 }
