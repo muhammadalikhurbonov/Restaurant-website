705de7d00cc59b51084b50057e00d83af4aacbf8
muhammadalikhurbonov <muhammadaliqurbonov551@gmail.com>
2023-04-04 07:40:13 +0500

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const Form = document.querySelector(".form");
 const Exit = document.querySelector(".exit_form");
-const inp = document.getElementById("username");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
 const Login = document.getElementById("login");
 
 function clickFunction() {
@@ -9,16 +10,17 @@ function clickFunction() {
   document.querySelector(".body").style.background = "black";
   document.getElementById("button").addEventListener("click", function (e) {
     e.preventDefault();
-    document.getElementById("login").innerHTML = inp.value;
+    document.getElementById("login").innerHTML = username.value;
     Form.classList.add("animation_time_off");
     document.querySelector(".dropdown-content").style.display = "block";
-    document.cookie = inp.value;
-    window.localStorage.setItem("Name", inp.value);
+    document.cookie = username.value;
+    window.localStorage.setItem("Username", username.value);
+    window.localStorage.setItem("Password", password.value);
   });
 }
-// Login.innerHTML = window.localStorage.getItem("Name");
+// Login.innerHTML = window.localStorage.getItem("Username");
 function logOut() {
-  Login.innerHTML = "Sign In";
+  Login.innerHTML = "Log In";
   window.localStorage.clear();
   document.querySelector(".dropdown-content").style.display = "none";
   document.querySelector(".body").style.background = "white";
