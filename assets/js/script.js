3f12e01a7f0131ee0b1c1d4e88d5a78cea09245e
muhammadalikhurbonov <muhammadaliqurbonov551@gmail.com>
2023-04-10 22:05:10 +0500

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,3 +75,40 @@ const rightMove = () => {
 
 btn_left.addEventListener("click", rightMove);
 btn_right.addEventListener("click", leftMove);
+
+const bttn_left = document.querySelector(".five_item_carousel_left");
+const bttn_right = document.querySelector(".five_item_carousel_right");
+const box = document.querySelector(".section_five_item_carousel_item");
+const boxes = document.querySelectorAll(".section_five_item_carousel  section_five_item_carousel_item");
+const Boxlegth = boxes.length - 1;
+let position_carousel = 0;
+
+function RemoveWholeClasses() {
+  for (const box of boxes) {
+    box.classList.remove("active");
+  }
+}
+const leftMove_carousel = () => {
+  RemoveWholeClasses();
+  if (position_carousel === Boxlegth) {
+    position_carousel = 0;
+  } else {
+    position_carousel++;
+  }
+  console.log(boxes[position_carousel]);
+  boxes[position_carousel].classList.add("active");
+};
+
+const rightMove_carousel = () => {
+  RemoveWholeClasses();
+  if (position_carousel === 0) {
+    position_carousel = Boxlegth;
+  } else {
+    position_carousel--;
+  }
+  console.log(boxes[position_carousel]);
+  boxes[position_carousel].classList.add("active");
+};
+
+bttn_left.addEventListener("click", rightMove_carousel);
+bttn_right.addEventListener("click", leftMove_carousel);
